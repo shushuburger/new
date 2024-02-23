9e575f70ba7aaf5b50cd9093f8f185265c73b4ef
김은수 <03robin@naver.com>
2024-02-23 16:45:55 +0900

    추가 : 메모 상세페이지로 이동 시 메모의 id 가져오도록

--- Front/js/content.js
+++ Front/js/content.js
@@ -1,1 +1,11 @@
-console.log(123);
+function getData() {
+    const saveData = JSON.parse(localStorage.getItem("memo"));
+}
+
+function getQueryString() {
+    const qs = window.location.search;
+
+    console.log(qs);
+}
+
+getQueryString();
