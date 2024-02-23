9758ae4d76d0db92ace7c002f2d9d2711289e8fc
김은수 <03robin@naver.com>
2024-02-23 16:15:43 +0900

    추가 : 메모 내용 로컬스토리지에 저장하기

--- Front/js/note.js
+++ Front/js/note.js
@@ -1,1 +1,20 @@
-console.log(123);
+const createButton = document.querySelector(".create-btn");
+
+createButton.addEventListener("click", function (event) {
+    const title = document.querySelector(".title");
+    const description = document.querySelector(".description");
+    const content = document.querySelector(".content");
+
+    console.log(title.value, description.value, content.value);
+
+    const now = new Date();
+
+    const saveValue = {
+        title: title.value,
+        description: description.value,
+        content: content.value,
+        createdAt: now.toLocaleDateString(),
+    };
+
+    localStorage.setItem("memo", JSON.stringify(saveValue));
+})
