65cfc83cf1748d30c3b3fe75646fac75130736e2
김은수 <03robin@naver.com>
2024-02-23 16:34:37 +0900

    개선 : 로컬스토리지에 있는 수만큼 첫 화면에 메모 자동 생성

--- Front/js/index.js
+++ Front/js/index.js
@@ -1,1 +1,53 @@
-console.log(123);
+{/* <li>
+<a href="./view/20230101.html">
+    <h1 class="title">테스트1</h1>
+</a>
+<h2 class="description">테스트2</h2>
+<div class="createdAt">
+    <span>2023.01.01</span>
+</div>
+</li> */}
+
+function getData() {
+    const saveData = JSON.parse(localStorage.getItem("memo"));
+    const memoWrapper = document.querySelector(".memo-container");
+    console.log(memoWrapper);
+
+    for(let i = 0; i< saveData.length; i++){
+        const list = drawMemo();
+        memoWrapper.appendChild(list);
+    }
+}
+
+function drawMemo() {
+    const li = document.createElement("li");
+
+    const a = document.createElement("a");
+    a.href = "";
+
+    const h1 = document.createElement("h1");
+    h1.className = "title";
+    h1.textContent = "테스트";
+
+    a.appendChild(h1);
+
+    const h2 = document.createElement("h2");
+    h2.className = "description";
+    h2.textContent = "테스트2";
+
+    const div = document.createElement("div");
+    div.className = "createdAt";
+
+    const span = document.createElement("span");
+    span.textContent = "2023.01.01";
+
+    div.appendChild(span);
+
+    li.appendChild(a);
+    li.appendChild(h2);
+    li.appendChild(div);
+
+    return li;
+}
+
+getData();
