f7ddc47d9a7e9aaad758ca4931343de3fa8e657f
김은수 <03robin@naver.com>
2024-02-23 16:54:55 +0900

    추가 : 메모에 저장되어 있는 내용을 연동시켜서 메모 상세페이지에 뜨도록 완료

--- Front/js/content.js
+++ Front/js/content.js
@@ -1,13 +1,35 @@
 function getData() {
     const saveData = JSON.parse(localStorage.getItem("memo"));
     const id = getQueryString();
+
+    let data;
+    for(let i = 0; i<saveData.length; i++) {
+        if(saveData[i].id === Number(id)) {
+            data = saveData[i];
+        }
+    }
+
+    renderData(data);
+    console.log(data);
 }
 
 function getQueryString() {
     const qs = window.location.search;
     const qs2 = new URLSearchParams(qs);
 
-    return qs2.id;
+    return qs2.get("id");
 }
 
-getQueryString();
+function renderData(memo) {
+    const title = document.querySelector(".title");
+    const description = document.querySelector(".description");
+    const createdAt = document.querySelector(".createdAt > span");
+    const content = document.querySelector(".content");
+
+    title.textContent = memo.title;
+    description.textContent = memo.description;
+    createdAt.textContent = memo.createdAt;
+    content.textContent = memo.content;
+}
+
+getData();
