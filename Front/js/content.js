3416b905c0412f1fd01c0a10f099859acb944a16
김은수 <03robin@naver.com>
2024-02-23 16:47:17 +0900

    개선 : id만 불러오기

--- Front/js/content.js
+++ Front/js/content.js
@@ -1,11 +1,13 @@
 function getData() {
     const saveData = JSON.parse(localStorage.getItem("memo"));
+    const id = getQueryString();
 }
 
 function getQueryString() {
     const qs = window.location.search;
+    const qs2 = new URLSearchParams(qs);
 
-    console.log(qs);
+    return qs2.id;
 }
 
 getQueryString();
